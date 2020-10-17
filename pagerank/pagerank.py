2e27bef3b524c5a7f9d7c4bf18d46168de12d2f7
Luis Sanchez <loic.quiros@gmail.com>
2020-10-16 18:57:49 -0700

    iterate_pagerank implementation

--- pagerank/pagerank.py
+++ pagerank/pagerank.py
@@ -12,17 +12,17 @@ def main():
         sys.exit("Usage: python pagerank.py corpus")
  
     corpus = crawl(sys.argv[1])
+
     # corpus2 = {"1.html": {"2.html", "3.html"}, "2.html": {"3.html"}, "3.html": {"2.html"}}
     # print(corpus2)
-    print(corpus)
+    # print(corpus)
 
     ranks = sample_pagerank(corpus, DAMPING, SAMPLES)
-    # sys.exit()
-
     print(f"PageRank Results from Sampling (n = {SAMPLES})")
     for page in sorted(ranks):
         print(f"  {page}: {ranks[page]:.4f}")
-    sys.exit()
+
+    # sys.exit()
 
     ranks = iterate_pagerank(corpus, DAMPING)
     print(f"PageRank Results from Iteration")
@@ -76,6 +76,7 @@ def transition_model(corpus, page, damping_factor):
         trans[filename] += round((1 - damping_factor) / len(corpus), 4)
     return trans
 
+
 def sample_pagerank(corpus, damping_factor, n):
     """
     Return PageRank values for each page by sampling `n` pages
@@ -85,29 +86,27 @@ def sample_pagerank(corpus, damping_factor, n):
     their estimated PageRank value (a value between 0 and 1). All
     PageRank values should sum to 1.
     """
-    samples = dict()
-    # n = 10
+    # Initialize the ranks dictionary with zero for all the pages. 
+    ranks = dict()
+    for page in corpus:
+        ranks[page] = 0
+    
+    # Generate samples and keep track of how many times each page has shown up.
     for x in range(n):
+        # Random start page.
         if x == 0:
-            for page in corpus:
-                samples[page] = 1 / len(corpus)
             current_page = random.choice(list(corpus))
-            # print(current_page)
+        # Chose random page based on the transition distribution.
         else:
             trans = transition_model(corpus, current_page, DAMPING)
-            # print(trans)
-            for page in samples:
-                samples[page] += trans[page]
-            current_page = random.choice(list(corpus))
-    print(samples)
-    total = 0.0
-    for page in samples:
-        samples[page] = samples[page] / n
-        total += samples[page]
-    print(total)
-
-    return samples
+            current_page = random.choices(list(trans.keys()), weights=trans.values(), k=1)[0]
+        ranks[current_page] += 1
     
+    # normalize probabilities
+    for page in ranks:
+        ranks[page] /= n
+
+    return ranks
 
 
 def iterate_pagerank(corpus, damping_factor):
@@ -119,7 +118,54 @@ def iterate_pagerank(corpus, damping_factor):
     their estimated PageRank value (a value between 0 and 1). All
     PageRank values should sum to 1.
     """
-    raise NotImplementedError
+    pre_ranks = dict()
+    new_ranks = dict()
+    for page in corpus:
+        pre_ranks[page] = 1 / len(corpus)
+
+    split = round((1 - damping_factor) / len(corpus), 4)
+    # num = 0    
+    while True:
+        for ranked in pre_ranks:
+            links = 0
+            for page in corpus:
+                if ranked in corpus[page]:
+                    links += pre_ranks[page] / len(corpus[page])
+            links *= damping_factor
+            new_ranks[ranked] = split + links
+        
+        # print(pre_ranks)
+        # print(ranks)
+        
+        if convergence(pre_ranks, new_ranks):
+            # print(num)
+            break
+        # num += 1
+        for ranked in pre_ranks:
+            pre_ranks[ranked] = new_ranks[ranked]
+    
+    # # normalize probabilities
+    # total = 0
+    # norm = 0
+    # for ranked in pre_ranks:
+    #     total += pre_ranks[ranked]
+    # for ranked in pre_ranks:
+    #     pre_ranks[ranked] /= total
+    #     norm += pre_ranks[ranked]
+    
+    # print(norm)
+    # print(pre_ranks)
+    # print(ranks)
+    return pre_ranks
+
+def convergence(pre_ranks, new_ranks):
+    for ranked in pre_ranks:
+        if pre_ranks[ranked] - new_ranks[ranked] > 0.001:
+            return False 
+        if pre_ranks[ranked] - new_ranks[ranked] < -0.001:
+            return False 
+    # if num > 8:
+    return True
 
 
 if __name__ == "__main__":
