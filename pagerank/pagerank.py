ba922fbb70709047976d54266f7480e962fc66fb
Luis Sanchez <loic.quiros@gmail.com>
2020-10-19 18:16:29 -0700

    implement the page-with-no-links constraint and  add some comments to the iterate_pagerank function

--- pagerank/pagerank.py
+++ pagerank/pagerank.py
@@ -13,16 +13,10 @@ def main():
  
     corpus = crawl(sys.argv[1])
 
-    # corpus2 = {"1.html": {"2.html", "3.html"}, "2.html": {"3.html"}, "3.html": {"2.html"}}
-    # print(corpus2)
-    # print(corpus)
-
     ranks = sample_pagerank(corpus, DAMPING, SAMPLES)
     print(f"PageRank Results from Sampling (n = {SAMPLES})")
     for page in sorted(ranks):
         print(f"  {page}: {ranks[page]:.4f}")
-
-    # sys.exit()
 
     ranks = iterate_pagerank(corpus, DAMPING)
     print(f"PageRank Results from Iteration")
@@ -69,6 +63,7 @@ def transition_model(corpus, page, damping_factor):
     a link at random chosen from all pages in the corpus.
     """
     trans = dict()
+    
     for filename in corpus:
         trans[filename] = 0.0
         if filename in corpus[page]:
@@ -86,23 +81,23 @@ def sample_pagerank(corpus, damping_factor, n):
     their estimated PageRank value (a value between 0 and 1). All
     PageRank values should sum to 1.
     """
-    # Initialize the ranks dictionary with zero for all the pages. 
+    # Initialize the ranks dictionary with zero for all the pages
     ranks = dict()
     for page in corpus:
         ranks[page] = 0
     
-    # Generate samples and keep track of how many times each page has shown up.
+    # Generate samples and keep track of how many times each page has shown up
     for x in range(n):
-        # Random start page.
+        # Random start page
         if x == 0:
             current_page = random.choice(list(corpus))
-        # Chose random page based on the transition distribution.
+        # Chose a page pseudorandomly based on the transition distribution
         else:
             trans = transition_model(corpus, current_page, DAMPING)
             current_page = random.choices(list(trans.keys()), weights=trans.values(), k=1)[0]
         ranks[current_page] += 1
     
-    # normalize probabilities
+    # normalize the probabilities
     for page in ranks:
         ranks[page] /= n
 
@@ -118,53 +113,48 @@ def iterate_pagerank(corpus, damping_factor):
     their estimated PageRank value (a value between 0 and 1). All
     PageRank values should sum to 1.
     """
-    pre_ranks = dict()
-    new_ranks = dict()
+    # Initialize the dictionary to keep track of current ranks
+    # Begin by assigning each page a rank of 1 / N
+    curr_ranks = dict()
     for page in corpus:
-        pre_ranks[page] = 1 / len(corpus)
+        curr_ranks[page] = 1 / len(corpus)
 
-    split = round((1 - damping_factor) / len(corpus), 4)
-    # num = 0    
+    # Initialize the dictionary to calculate new ranks
+    new_ranks = dict()
+
+    # The 1 - d probability of choosing a page at random is split evenly among all N possible pages
+    split_prob = round((1 - damping_factor) / len(corpus), 4)
+    
+    # Calculate a new set of PageRank values for each page until convergence
     while True:
-        for ranked in pre_ranks:
-            links = 0
+        for ranked in curr_ranks:
+            links_prob = 0
+            # The d probability of choosing a page based on the PageRanks of all pages that link to it
             for page in corpus:
                 if ranked in corpus[page]:
-                    links += pre_ranks[page] / len(corpus[page])
-            links *= damping_factor
-            new_ranks[ranked] = split + links
-        
-        # print(pre_ranks)
-        # print(ranks)
-        
-        if convergence(pre_ranks, new_ranks):
-            # print(num)
+                    links_prob += curr_ranks[page] / len(corpus[page])
+                # A page that has no links is interpreted as having links for every page including itself
+                elif len(corpus[page]) == 0:
+                    links_prob += curr_ranks[page] / len(corpus)
+            links_prob *= damping_factor
+            # Adding up the two probabilities
+            new_ranks[ranked] = split_prob + links_prob
+        # Compare current rank values and the new rank values
+        if convergence(curr_ranks, new_ranks):
             break
-        # num += 1
-        for ranked in pre_ranks:
-            pre_ranks[ranked] = new_ranks[ranked]
+        # Update rank values
+        for ranked in curr_ranks:
+            curr_ranks[ranked] = new_ranks[ranked]
     
-    # # normalize probabilities
-    # total = 0
-    # norm = 0
-    # for ranked in pre_ranks:
-    #     total += pre_ranks[ranked]
-    # for ranked in pre_ranks:
-    #     pre_ranks[ranked] /= total
-    #     norm += pre_ranks[ranked]
-    
-    # print(norm)
-    # print(pre_ranks)
-    # print(ranks)
-    return pre_ranks
+    return curr_ranks
 
-def convergence(pre_ranks, new_ranks):
-    for ranked in pre_ranks:
-        if pre_ranks[ranked] - new_ranks[ranked] > 0.001:
+# Convergence occurs when no PageRank value changes by more than 0.001 between iterations
+def convergence(curr_ranks, new_ranks):
+    for ranked in curr_ranks:
+        if curr_ranks[ranked] - new_ranks[ranked] > 0.001:
             return False 
-        if pre_ranks[ranked] - new_ranks[ranked] < -0.001:
+        elif curr_ranks[ranked] - new_ranks[ranked] < -0.001:
             return False 
-    # if num > 8:
     return True
 
 
